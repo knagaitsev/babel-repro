44ac848dba0dc356f50cb75d138c5e57e8f51b90
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-06-17 19:11:27 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+    entry: './index.js',
+    module: {
+        rules: [
+            // {
+            //     test: /\.js$/,
+            //     use: [
+            //         {
+            //             loader: 'babel-loader',
+            //             options: {
+            //                 presets: ['@babel/preset-env'],
+            //             },
+            //         },
+            //     ],
+            // }
+        ],
+    },
+};
